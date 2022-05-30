5a5abb3bbfcdb5be49e6b9c731e4bca5dd2afb3d
Indeed <gravitybartek@gmail.com>
2022-05-30 18:42:10 +0200

    Fixed mouse position, added Rectangle drawing

--- canvas.ts
+++ canvas.ts
@@ -1,28 +1,76 @@
 ﻿const canvas = <HTMLCanvasElement>document.getElementById("drawingSpace");
-const ctx = canvas.getContext('2d');
+const ctx = canvas.getContext("2d");
 ctx.strokeStyle = '#000000';
 ctx.lineWidth = 5;
 
 let prevX;
 let prevY;
+
+let mode = "line";
+//line
+//rectFill
+//rectStroke
+
+//region cursor preview
+let cursor = document.getElementById("cursor");
+document.body.addEventListener("mousemove", function (e) {
+    cursor.style.left = e.clientX + "px";
+    cursor.style.top = e.clientY + "px";
+});
+//endregion
+
+console.log(canvas.offsetLeft);
+console.log(canvas.offsetTop);
+
+function  getMousePos(canvas, evt) {
+    let rect = canvas.getBoundingClientRect(), // abs. size of element
+        scaleX = canvas.width / rect.width,    // relationship bitmap vs. element for x
+        scaleY = canvas.height / rect.height;  // relationship bitmap vs. element for y
+
+    return {
+        x: (evt.clientX - rect.left) * scaleX,   // scale mouse coordinates after they have
+        y: (evt.clientY - rect.top) * scaleY     // been adjusted to be relative to element
+    }
+}
+
 canvas.addEventListener("mousedown", (e) => {
-    prevX = Math.floor(e.offsetX /4.5)
-    prevY = Math.floor(e.offsetY /4.5);
-    canvas.addEventListener("mousemove", draw);
+    let pos = getMousePos(canvas, e);
+    prevX = pos.x;
+    prevY = pos.y;
+    if (mode == "line") {
+        canvas.addEventListener("mousemove", draw);
+    }
+    // else if(mode.startsWith("rect")) just set the prevX and prevY to the corner of the rectangle
 });
-
 canvas.addEventListener("mouseup", (e) => {
-    canvas.removeEventListener("mousemove", draw);
+    if (mode == "line") {
+        canvas.removeEventListener("mousemove", draw);
+    } else if (mode.startsWith("rect")) {
+        let pos = getMousePos(canvas, e);
+        let x = pos.x;
+        let y = pos.y;
+        ctx.beginPath();
+        ctx.rect(prevX, prevY, x - prevX, y - prevY);
+        ctx.closePath();
+        if (mode.endsWith("Fill")) {
+            ctx.fill();
+        } else if (mode.endsWith("Stroke")) {
+            ctx.stroke();
+        }
+    }
 });
 
-function draw(e : MouseEvent){
-    let x = Math.floor(e.offsetX /4.5)
-    let y = Math.floor(e.offsetY /4.5);
+function draw(e) {
+    let pos = getMousePos(canvas, e);
+    let x = pos.x;
+    let y = pos.y;
     ctx.beginPath();
-    ctx.moveTo(x,y);
+    ctx.moveTo(x, y);
     ctx.lineTo(prevX, prevY);
     ctx.closePath();
     ctx.stroke();
-    prevX = Math.floor(e.offsetX /4.5)
-    prevY = Math.floor(e.offsetY /4.5);
+    prevX = x;
+    prevY = y;
 }
+
+//# sourceMappingURL=canvas.js.map
